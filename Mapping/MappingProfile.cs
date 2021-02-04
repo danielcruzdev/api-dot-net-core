e15a21e06977cda9d024a82862a576aa5bbfb962
Daniel Cruz <d.afc98@gmail.com>
2021-02-04 00:03:09 -0300

    configuring mapping between entities and view models

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,4 +1,8 @@
-﻿using AutoMapper;
+﻿using api_dot_net_core.Entities.BaseExcel;
+using api_dot_net_core.Entities.RCompras;
+using api_dot_net_core.ViewModels.RCompras;
+using api_dot_net_core.ViewModels.Shared;
+using AutoMapper;
 using Entity;
 using ViewModel;
 
@@ -8,8 +12,20 @@ namespace Mapping
     {
         public MappingProfile()
         {
+            //Base Excel Cabeçalho
+            CreateMap<BaseCabecalho, BaseExcelHeader>();
+
+            //Pessoa
             CreateMap<Pessoa, PessoaViewModel>();
             CreateMap<PessoaViewModel, Pessoa>();
+
+            //Relatório Compras
+            CreateMap<RCompraCabecalho, RCompraCabecalhoViewModel>();
+            CreateMap<RCompra, RCompraViewModel>();
+            CreateMap<RCompraCliente, RCompraClienteViewModel>();
+            CreateMap<RCompraEmpresa, RCompraEmpresaViewModel>();
+            CreateMap<RCompraCategoria, RCompraCategoriaViewModel>();
+            CreateMap<RCompraProduto, RCompraProdutoViewModel>();
         }
     }
 }
