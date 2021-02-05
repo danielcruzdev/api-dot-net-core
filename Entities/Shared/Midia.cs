5bd2cd8250c5903f525a296aa5c09949c1cd9ada
Daniel Cruz <d.afc98@gmail.com>
2021-02-04 23:52:15 -0300

    add model to midia

---
+++ Entities/Shared/Midia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace api_dot_net_core.Entities.Shared
+{
+    public class Midia
+    {
+        public Midia()
+        {
+            Guid = System.Guid.NewGuid();
+        }
+
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Content { get; set; }
+        public Guid? Guid { get; set; }
+        public string ContentType { get; set; }
+    }
+}
